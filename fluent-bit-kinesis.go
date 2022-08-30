7d9433a513f080737c4bcd3e889510c08503b2d9
Foad Nosrati Habibi <foad.nh@gmail.com>
2022-08-30 13:44:54 +1000

    V4

--- fluent-bit-kinesis.go
+++ fluent-bit-kinesis.go
@@ -263,7 +263,7 @@ func unpackRecords(kinesisOutput *kinesis.OutputPlugin, data unsafe.Pointer, len
 		}
 		for k, v := range record {
 			fmt.Printf("# %+v: %+v\n", k, string(v.([]byte)))
-			logrus.Infof("# %+v: %+v", k, string(v.([]byte)))
+			logrus.Infof("$ %+v: %+v", k, string(v.([]byte)))
 		}
 		continue
 
