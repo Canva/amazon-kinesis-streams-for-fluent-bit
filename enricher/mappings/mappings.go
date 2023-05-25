f5ef1236cf551fb87d0d65e78b1525a7f56b78fa
Mathew Vo <mathew.v@canva.com>
2023-05-25 18:23:58 +1000

    done

--- enricher/mappings/mappings.go
+++ enricher/mappings/mappings.go
@@ -5,19 +5,22 @@ const (
 )
 
 const (
-	LOG_FIELD_NAME = "log"
-	MSG_FIELD_NAME = "message"
+	LOG_FIELD_NAME       = "log"
+	MSG_FIELD_NAME       = "message"
+	TRANSPORT_FIELD_NAME = "transport"
 )
 
 const (
 	KUBERNETES_RESOURCE_FIELD_NAME          = "kubernetes"
 	KUBERNETES_RESOURCE_CLUSTER_NAME        = "cluster.name"
 	KUBERNETES_CONTAINER_NAME               = "container_name"
+	KUBERNETES_RESOURCE_NODE_NAME           = "node.name"
 	PLACEHOLDER_MISSING_KUBERNETES_METADATA = "_missing_metadata"
 )
 
 const (
 	RESOURCE_FIELD_NAME             = "resource"
+	RESOURCE_SERVICE_NAME           = "service.name"
 	RESOURCE_PARTITION              = "cloud.partition"
 	RESOURCE_ACCOUNT_ID             = "cloud.account.id"
 	RESOURCE_ACCOUNT_NAME           = "cloud.account.name"
@@ -34,6 +37,7 @@ const (
 	ENV_REGION                 = "CLOUD_REGION"
 	ENV_ACCOUNT_GROUP_FUNCTION = "CLOUD_ACCOUNT_GROUP_FUNCTION"
 	ENV_CLUSTER_NAME           = "K8S_CLUSTER_NAME"
+	ENV_NODE_NAME              = "K8S_NODE_NAME"
 	ENV_PARTITION              = "CLOUD_PARTITION"
 	ENV_ORGANISATION           = "ORGANIZATION"
 	ENV_PLATFORM               = "CLOUD_PLATFORM"
